8b25f7d4f3802b1e06455e9dc13e4ef3fd83f50d
Ayman <aymn.abdellatief@gmail.com>
2020-11-20 01:59:37 +0200

    Adding Navigation to the Site

--- BethaynsPieShop/Controllers/PieController.cs
+++ BethaynsPieShop/Controllers/PieController.cs
@@ -29,5 +29,13 @@ namespace BethaynsPieShop.Controllers
             piesListViewModel.CurrentCategory = "Chees Cakes";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+        }
     }
 }
